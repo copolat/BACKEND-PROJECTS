15fc16cace8ca64c4c35ffb366ceae22d08c982d
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-02-08 20:50:03 -0600

    Complete simple node server that changes HTML text according to url

--- 02-Hello World/server.js
+++ 02-Hello World/server.js
@@ -5,7 +5,20 @@ const port = 5000;
 const requestListener = function(req, res) {
   
   res.setHeader("Content-Type", "text/html");
-  res.end(`Hello World!`);
+  if (req.url === '/student'){
+    res.end(`This is STUDENT page`);
+  }else if (req.url === '/teacher'){
+    res.end(`This is TEACHER page`);
+  }else if (req.url === '/principal'){
+    res.end(`This is PRINCIPAL page`);
+  }else if (req.url === '/admin'){
+    res.end(`This is ADMIN page`);
+  }else if (req.url === '/data'){
+    res.setHeader("Content-Type", "application/json");
+    res.end(`{"message": "This is a message from webserver"}`);
+  }else {
+    res.end(`Hello World!`);
+  }
 }
 
 const server = http.createServer(requestListener);
