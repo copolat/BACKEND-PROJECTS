20c342f5aade416a817e1e4396636d9226823c2e
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-02-08 21:39:07 -0600

    Make requests with request()

--- 03-HTTP-Client/getRequest.js
+++ 03-HTTP-Client/getRequest.js
@@ -3,7 +3,36 @@ const https = require( 'https' );
 // get() function format
 // https.get(URL_String, Callback_Function) {    Action	}
 
-let request = https.get('https://jsonplaceholder.typicode.com/users?_limit=2', (res) => {
+// let request = https.get('https://jsonplaceholder.typicode.com/users?_limit=2', (res) => {
+//   if (res.statusCode !== 200) {
+//     console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+//     res.resume();
+//     return;
+//   };
+//   let data = '';
+
+//     res.on('data', (chunk) => {
+//       data += chunk;
+//     });
+
+//     res.on('close', () => {
+//       console.log('Retrieved all data');
+//       console.log(JSON.parse(data));
+//     });
+
+//     request.on('error', (err) => {
+//       console.error(`Encountered an error trying to make a request: ${err.message}`);
+//     });
+//   });  
+
+// reuest() method
+// https.request(URL_String, Options_Object, Callback_Function) {  Action  }
+
+const options = {
+  method: 'GET'
+};
+
+let request = https.request('https://jsonplaceholder.typicode.com/users?_limit=2', options,  (res) => {
   if (res.statusCode !== 200) {
     console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
     res.resume();
@@ -14,13 +43,12 @@ let request = https.get('https://jsonplaceholder.typicode.com/users?_limit=2', (
     res.on('data', (chunk) => {
       data += chunk;
     });
-
     res.on('close', () => {
       console.log('Retrieved all data');
       console.log(JSON.parse(data));
     });
-
-    request.on('error', (err) => {
-      console.error(`Encountered an error trying to make a request: ${err.message}`);
-    });
   });  
+  request.end(`{"message": "This is a JSON response"}`);
+  request.on('error', (err) => {
+    console.error(`Encountered an error trying to make a request: ${err.message}`);
+  });
